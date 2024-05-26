de641c66bc01448107fe234ec093b4ea7828dfed
Enmanuel <enmareco.co@gmail.com>
2024-05-26 20:40:54 -0300

    cambio port

--- api/config.js
+++ api/config.js
@@ -6,4 +6,4 @@ export const DB_NAME = process.env.DB_NAME || 'adorandoalrey'
 export const DB_PASSWORD= process.env.DB_PASSWORD || 'Enma2707'
 export const DB_PORT= process.env.DB_PORT || 5432
 
-export const PORT = process.env.PORT || 8800
+export const PORT = process.env.PORT || 4000
