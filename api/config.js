6a960a60aac3b47f6ce7750919ea333f4dc3fa6d
Enmanuel <enmareco.co@gmail.com>
2024-05-27 16:05:08 -0300

    add db_url

--- api/config.js
+++ api/config.js
@@ -1,5 +1,6 @@
 export const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173"
 
+
 export const DB_HOST= process.env.DB_HOST || 'localhost'
 export const DB_USER= process.env.DB_USER ||'postgres'
 export const DB_NAME = process.env.DB_NAME || 'adorandoalrey'
