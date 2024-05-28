99d9bfb566325f1471d6588d37f9cf11a649de25
Enmanuel <enmareco.co@gmail.com>
2024-05-28 17:36:02 -0300

    funtional

--- api/db.js
+++ api/db.js
@@ -1,7 +1,7 @@
-import { Sequelize, HasMany } from "sequelize";
-import {DB_HOST, DB_PORT,DB_USER, DB_PASSWORD, DB_NAME, DB_URL } from './config.js'
+import { Sequelize} from "sequelize";
+import {DB_HOST, DB_PORT,DB_USER, DB_PASSWORD, DB_NAME, } from './config.js'
 
-export const sequelize = new Sequelize( DB_URL ||
+export const sequelize = new Sequelize(   
   DB_NAME,
   DB_USER,
   DB_PASSWORD,
