ac7e95803cda361634ef26ba582d36dcea7c37d2
Enmanuel <enmareco.co@gmail.com>
2024-05-28 16:00:28 -0300

    put internal db url

--- api/config.js
+++ api/config.js
@@ -6,4 +6,4 @@ export const DB_USER= process.env.DB_USER ||'postgres'
 export const DB_NAME = process.env.DB_NAME || 'adorandoalrey'
 export const DB_PASSWORD= process.env.DB_PASSWORD || 'Enma2707'
 export const DB_PORT= process.env.DB_PORT || 5432
-
+export const DB_URL = process.env.DB_URL
--- api/db.js
+++ api/db.js
@@ -1,7 +1,7 @@
 import { Sequelize, HasMany } from "sequelize";
-import {DB_HOST, DB_PORT,DB_USER, DB_PASSWORD, DB_NAME, } from './config.js'
+import {DB_HOST, DB_PORT,DB_USER, DB_PASSWORD, DB_NAME, DB_URL } from './config.js'
 
-export const sequelize = new Sequelize(
+export const sequelize = new Sequelize( DB_URL ||
   DB_NAME,
   DB_USER,
   DB_PASSWORD,
