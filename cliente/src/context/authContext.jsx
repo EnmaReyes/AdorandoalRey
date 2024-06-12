6b10065e84c9cae5ae142ee149eb20b846f7c5d3
Enmanuel <enmareco.co@gmail.com>
2024-06-12 16:46:20 -0300

    confing time to logout

--- cliente/src/context/authContext.jsx
+++ cliente/src/context/authContext.jsx
@@ -1,31 +1,77 @@
-import axios from 'axios'
-import React, { useEffect, useState } from 'react'
-import { createContext } from 'react'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { createContext } from "react";
 
-export const AuthContext = createContext()
+export const AuthContext = createContext();
 const URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:8800";
 
-export const AuthContextProvider = ({children})=>{
-    const [currentUser, setCurrentUser]= useState(JSON.parse(localStorage.getItem("user")) || null)
-    const [showUserEdit, setShowUserEdit] = useState(true);
+export const AuthContextProvider = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState(
+    JSON.parse(localStorage.getItem("user")) || null
+  );
+  const [showUserEdit, setShowUserEdit] = useState(true);
+  const [logoutTimer, setLogoutTimer] = useState(null);
+  const [isUserActive, setIsUserActive] = useState(false);
 
-    const login = async (inputs)=>{
-        const res = await axios.post(`${URL}/api/auth/login`, inputs, { withCredentials: true });
-        setCurrentUser(res.data)
+  const login = async (inputs) => {
+    const res = await axios.post(`${URL}/api/auth/login`, inputs, {
+      withCredentials: true,
+    });
+    setCurrentUser(res.data);
+  };
+
+  const logout = async () => {
+    await axios.post(`${URL}/api/auth/logout`, null, { withCredentials: true });
+    setCurrentUser(null);
+  };
+
+  // Función para reiniciar el temporizador de cierre de sesión
+  const resetLogoutTimer = () => {
+    if (logoutTimer) {
+      clearInterval(logoutTimer); //! limpiar el temporizador anteriror
+    }
+    // Establecer un nuevo temporizador de 1 hora (3600000 ms)
+    const timer = setTimeout(() => {
+      logout();
+    }, 3600000);
+    setLogoutTimer(timer);
+  };
+
+  // Función para manejar la actividad del usuario
+  const handleUserActivity = () => {
+    setIsUserActive(true);
+    resetLogoutTimer();
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleUserActivity);
+    document.addEventListener("mousemove", handleUserActivity);
+    document.addEventListener("keydown", handleUserActivity);
+    document.addEventListener("keypress", handleUserActivity);
+
+    return () => {
+      document.removeEventListener("mousedown", handleUserActivity);
+      document.removeEventListener("mousemove", handleUserActivity);
+      document.removeEventListener("keydown", handleUserActivity);
+      document.removeEventListener("keypress", handleUserActivity);
     };
+  }, []);
 
-    const logout = async ()=>{
-        await axios.post(`${URL}/api/auth/logout`, null, { withCredentials: true } );
-        setCurrentUser(null)
-    };
+  useEffect(() => {
+    if (currentUser) {
+      resetLogoutTimer();
+    }
+  }, [currentUser]);
 
-    useEffect(()=>{
-        localStorage.setItem("user", JSON.stringify(currentUser))
-    },[currentUser]);
+  useEffect(() => {
+    localStorage.setItem("user", JSON.stringify(currentUser));
+  }, [currentUser]);
 
-    return(
-        <AuthContext.Provider value={{currentUser, login, logout, showUserEdit, setShowUserEdit}}>
-            {children}
-        </AuthContext.Provider>
-    )
-}
+  return (
+    <AuthContext.Provider
+      value={{ currentUser, login, logout, showUserEdit, setShowUserEdit }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
