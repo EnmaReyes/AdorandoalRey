d87dc7178e3c381f9393cbde7d6f2b73cfc8c258
Enmanuel <enmareco.co@gmail.com>
2024-08-28 14:11:57 -0300

    register page

--- cliente/src/pages/Register.jsx
+++ cliente/src/pages/Register.jsx
@@ -1,8 +1,16 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
-
+import "./Register.scss";
+import logo from "../assets/logoblanco.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faArrowLeft,
+  faEye,
+  faEyeSlash,
+} from "@fortawesome/free-solid-svg-icons";
 const URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:4000";
+
 const Register = () => {
   const [inputs, setInputs] = useState({
     email: "",
@@ -10,9 +18,20 @@ const Register = () => {
     password: "",
   });
   const [error, setError] = useState(null);
-
+  const [focus, setFocus] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
   const navigate = useNavigate();
 
+  const hadleFocus = () => {
+    setFocus(true);
+  };
+  const handleBlur = () => {
+    setFocus(false);
+  };
+
+  const togglePasswordVisibility = () => {
+    setShowPassword((prevShowPassword) => !prevShowPassword);
+  };
   const handlechange = (e) => {
     setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
   };
@@ -28,36 +47,64 @@ const Register = () => {
   };
 
   return (
-    <div className="auth">
-      <h1>Registrar</h1>
-      <form>
-        <input
-          required
-          type="email"
-          placeholder="email"
-          name="email"
-          onChange={handlechange}
-        />
-        <input
-          required
-          type="text"
-          placeholder="user name"
-          name="username"
-          onChange={handlechange}
-        />
-        <input
-          required
-          type="password"
-          placeholder="password"
-          name="password"
-          onChange={handlechange}
-        />
-        <button onClick={handlesubmit}>Registrar</button>
-        {error && <p>{error}</p>}
-        <span>
-          ¿posees cuenta? <Link to="/login">Iniciar Sesión</Link>
-        </span>
-      </form>
+    <div className="register-contain">
+      <div className="registro-box">
+        <div className={`left ${focus ? "reduced-width" : ""}`}>
+          <img src={logo} alt="Logo" />
+          <Link className="inicio" to="/">
+            <button>
+              <FontAwesomeIcon icon={faArrowLeft} />
+              <span> Inicio</span>
+            </button>
+          </Link>
+        </div>
+
+        <div className="rigth">
+          <div className="info-register">
+            <h1>Registrarse</h1>
+            <form>
+              <input
+                required
+                type="email"
+                placeholder="Email"
+                name="email"
+                onChange={handlechange}
+                onFocus={hadleFocus}
+                onBlur={handleBlur}
+              />
+              <input
+                required
+                type="text"
+                placeholder="Usuario"
+                name="username"
+                onChange={handlechange}
+                onFocus={hadleFocus}
+                onBlur={handleBlur}
+              />
+              <input
+                required
+                type={showPassword ? "text" : "password"}
+                placeholder="Contraseña"
+                name="password"
+                onChange={handlechange}
+                onFocus={hadleFocus}
+              />
+              <a onClick={togglePasswordVisibility} className="show-hide">
+                {showPassword ? (
+                  <FontAwesomeIcon icon={faEye} />
+                ) : (
+                  <FontAwesomeIcon icon={faEyeSlash} />
+                )}
+              </a>
+              <button onClick={handlesubmit}>Registrar</button>
+              {error && <p>{error}</p>}
+              <span>
+                ¿Posees cuenta? <Link to="/login">Iniciar Sesión</Link>
+              </span>
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
