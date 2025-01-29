d5ce317b6c06a526326bc22eb386b13d4fb6b762
Enmanuel <enmareco.co@gmail.com>
2025-01-29 19:38:05 -0300

    styling contactus site

--- cliente/src/components/ContactUS/contactus.jsx
+++ cliente/src/components/ContactUS/contactus.jsx
@@ -69,52 +69,77 @@ const Contactus = () => {
 
   return (
     <div className="contact-container">
+      <div class="custom-shape-divider-top-1738175201">
+        <svg
+          data-name="Layer 1"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 1200 120"
+          preserveAspectRatio="none"
+        >
+          <path
+            d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z"
+            class="shape-fill"
+          ></path>
+        </svg>
+      </div>
+      <div className="contact-block">
       <div className="contact-box">
+        <h1 className="titulo">Contactanos</h1>
         <form onSubmit={handlesubmit} className="form_contact">
-          <div className="fullname_inputs">
+          <div className="fullname_inputs ">
+            <div className="input-group">
+              <input
+                type="text"
+                id="firstName"
+                name="firstName"
+                className="input-group__input"
+                required
+                onChange={handleChange}
+              />
+              <label htmlFor="firstName" className="input-group__label">
+                Nombre
+              </label>
+            </div>
+            <div className="input-group">
+              <input
+                type="text"
+                id="lastName"
+                name="lastName"
+                className="input-group__input"
+                required
+                onChange={handleChange}
+              />
+              <label htmlFor="lastName" className="input-group__label">
+                Apellido
+              </label>
+            </div>
+          </div>
+          <div className="input-group">
             <input
               type="text"
-              id="firstName"
-              name="firstName"
+              id="email"
+              name="email"
               className="input-group__input"
               onChange={handleChange}
+              required
             />
-            <label htmlFor="firstName" className="input-group__label">
-              Nombre
-            </label>
-
-            <input
-              type="text"
-              id="lastName"
-              name="lastName"
-              className="input-group__input"
-              onChange={handleChange}
-            />
-            <label htmlFor="lastName" className="input-group__label">
-              Apellido
+            <label htmlFor="email" className="input-group__label">
+              Correo Electronico
             </label>
           </div>
-          <input
-            type="text"
-            id="email"
-            name="email"
-            className="input-group__input"
-            onChange={handleChange}
-          />
-          <label htmlFor="email" className="input-group__label">
-            Correo Electronico
-          </label>
-
-          <input
-            type="text"
-            id="phone"
-            name="phone"
-            className="input-group__input"
-            onChange={handleChange}
-          />
-          <label htmlFor="phone" className="input-group__label">
-            Numero Movíl
-          </label>
+          <div className="input-group">
+            <input
+              type="text"
+              id="phone"
+              name="phone"
+              className="input-group__input"
+              required
+              onChange={handleChange}
+            />
+            <label htmlFor="phone" className="input-group__label">
+              Numero Movíl
+            </label>
+          </div>
 
           <textarea
             className="contact-textarea"
@@ -122,12 +147,16 @@ const Contactus = () => {
             name="message"
             onChange={handleChange}
           />
+
           {error && <p className="error-message">{error}</p>}
-          <button type="submit">Enviar</button>
+          <button type="submit" className="emailbutton">Enviar</button>
         </form>
       </div>
 
-      <div></div>
+      <div className="info-container">
+        <div class="triangle-horizontal"></div>
+      </div>
+      </div>
     </div>
   );
 };
